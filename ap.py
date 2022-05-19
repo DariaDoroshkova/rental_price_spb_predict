9f8eab0994a134e9a4897f6535021b663f3ad97d
DariaDoroshkova <you@example.com>
2022-05-19 07:59:51 +0300

    midified model

--- ap.py
+++ ap.py
@@ -2,7 +2,7 @@ import pandas
 from flask import Flask, request
 import joblib
 import numpy
-MODEL_PATH = 'model.pkl'
+MODEL_PATH = 'model_2.pkl'
 
 ap = Flask(__name__)
 model = joblib.load(MODEL_PATH)
@@ -11,16 +11,14 @@ model = joblib.load(MODEL_PATH)
 def predict():
     args = request.args
     floor = args.get('floor', default=-1, type=int)
-    open_plan = args.get('open_plan', default=-1, type=int)
-    rooms = args.get('rooms', default=-1, type=int)
-    studio = args.get('studio', default=-1, type=int)
     area = args.get('area', default=-1, type=float)
-    kitchen_area = args.get('kitchen_area', default= -1, type=float)
+    kitchen_area = args.get('kitchen_area', default=-1, type=float)
+    renovation = args.get('renovation', default=-1, type=int)
+    agent_fee = args.get('agent_fee', default=-1, type=float)
+    rooms = args.get('rooms', default=-1, type=int)
 
-    # response = "open_plan:{}, rooms:{}, area:{}, renovation:{}".format(open_plan, rooms, area, renovation)
 
-
-    x = numpy.array([floor, open_plan, rooms, studio, area, kitchen_area]).reshape(1,-1)
+    x = numpy.array([floor, area, kitchen_area, renovation, agent_fee, rooms]).reshape(1,-1)
     result = model.predict(x)
     result = result.reshape(1,-1)
 
