4ff09aab801b84909ee1e24285c204b4c679ecbc
DariaDoroshkova <91206942+DariaDoroshkova@users.noreply.github.com>
2022-06-09 19:50:16 +0300

    Update ap.py

--- ap.py
+++ ap.py
@@ -2,10 +2,11 @@ import pandas
 from flask import Flask, request
 import joblib
 import numpy
-MODEL_PATH = 'model_2.pkl'
+MODEL_PATH_1 = 'model_1.pkl'
+MODEL_PATH_2 = 'model_2.pkl'
 
 ap = Flask(__name__)
-model = joblib.load(MODEL_PATH)
+
 
 @ap.route("/predict_price", methods = ['GET'])
 def predict():
@@ -16,13 +17,27 @@ def predict():
     renovation = args.get('renovation', default=-1, type=int)
     agent_fee = args.get('agent_fee', default=-1, type=float)
     rooms = args.get('rooms', default=-1, type=int)
+    model = args.get('model', default = 0, type=int)
 
+    if model == 1:
 
-    x = numpy.array([floor, area, kitchen_area, renovation, agent_fee, rooms]).reshape(1,-1)
-    result = model.predict(x)
-    result = result.reshape(1,-1)
+        model_1 = joblib.load(MODEL_PATH_1)
+        x = numpy.array([floor, area, kitchen_area, rooms]).reshape(1,-1)
+        result = model_1.predict(x)
+        result = result.reshape(1,-1)
 
-    return str(result[0][0])
+        return str(result[0][0])
+
+    if model == 2:
+        model_2 = joblib.load(MODEL_PATH_2)
+        x_2 = numpy.array([floor, area, kitchen_area, renovation, agent_fee, rooms]).reshape(1, -1)
+        result_2 = model_2.predict(x_2)
+        result_2 = result_2.reshape(1, -1)
+
+        return str(result_2[0][0])
+
+    else:
+        return "model type should be 1 or 2"
 
 
 if __name__ == '__main__':
